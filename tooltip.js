a09087757691cb177c464f3c79de557a09e40653
unknown <amandapunsammy@xennialconsulting.com>
2019-10-29 23:35:12 -0400

    adding style in html file

--- tooltip.js
+++ tooltip.js
@@ -24,10 +24,6 @@ class Tooltip extends HTMLElement {
   _showTooltip(){
     this._tooltipContainer = document.createElement("div");
     this._tooltipContainer.textContent = this._tooltipText;
-    this._tooltipContainer.style.backgroundColor = "black";
-    this._tooltipContainer.style.color = "white";
-    this._tooltipContainer.style.position = "absolute";
-    this._tooltipContainer.style.zIndex = "10";
     this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
