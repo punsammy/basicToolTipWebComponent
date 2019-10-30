667663196fd7efb33aed00165c766c6459294f42
unknown <amandapunsammy@xennialconsulting.com>
2019-10-29 22:11:27 -0400

    initial setup

---
+++ tooltip.js
@@ -0,0 +1,42 @@
+// Every custom element needs to extend HTMLElement
+class Tooltip extends HTMLElement {
+  constructor(){ // called when element is created, use this function for basic initializatoins
+    super();
+    this._tooltipContainer;
+    this._tooltipText = "Some dummy tooltip text";
+  }
+
+  connectedCallback() { // DOM initializations, called when elements are mounted to page
+    if (this.hasAttribute("text")) {
+      this._tooltipText = this.getAttribute("text");
+    }
+    const tooltipIcon = document.createElement("span");
+    tooltipIcon.textContent = " (?)";
+    tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
+    tooltipIcon.addEventListener("mouseleave", this._hideToolTip.bind(this));
+    this.appendChild(tooltipIcon);
+  }
+
+// Naming convention for functions called within a class
+  _showTooltip(){
+    this._tooltipContainer = document.createElement("div");
+    this._tooltipContainer.textContent = this._tooltipText;
+    this.appendChild(this._tooltipContainer);
+  }
+
+  _hideToolTip(){
+    this.removeChild(this._tooltipContainer);
+  }
+
+  // called when element is detached from DOM
+  // disconnectedCallback(){}
+
+  // observed attribute updated
+  // attributeChangedCallback(){}
+}
+
+
+// REGISTERING CUSTOM ELEMENT
+// RULE: needs to have 2 words separated by a dash, because all built-in HTML elements are single words
+// this prevents us from accidentallty overwriting existing HTML elements
+customElements.define("basic-tooltip", Tooltip);
