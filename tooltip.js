598f727d1bed2c37bbcd955652661d5089fc062e
unknown <amandapunsammy@xennialconsulting.com>
2019-10-30 01:39:30 -0400

    refactor tooltip toggle function

--- tooltip.js
+++ tooltip.js
@@ -2,7 +2,8 @@
 class Tooltip extends HTMLElement {
   constructor(){ // called when element is created, use this function for basic initializatoins
     super();
-    this._tooltipContainer;
+    this._tooltipIcon;
+    this._tooltipVisible = false;
     this._tooltipText = "Some dummy tooltip text";
     this.attachShadow({mode: "open" });
     const template = document.querySelector("#tooltip-template");
@@ -13,11 +14,11 @@ class Tooltip extends HTMLElement {
     if (this.hasAttribute("text")) {
       this._tooltipText = this.getAttribute("text");
     }
-    const tooltipIcon = this.shadowRoot.querySelector("span");
-    tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
-    tooltipIcon.addEventListener("mouseleave", this._hideToolTip.bind(this));
-    this.shadowRoot.appendChild(tooltipIcon);
-    this.style.position = "relative";
+    this._tooltipIcon = this.shadowRoot.querySelector("span");
+    this._tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
+    this._tooltipIcon.addEventListener("mouseleave", this._hideToolTip.bind(this));
+    this.shadowRoot.appendChild(this._tooltipIcon);
+    this._render();
   }
 
   // observed attribute updates
@@ -39,17 +40,33 @@ class Tooltip extends HTMLElement {
   // called when element is detached from DOM
   disconnectedCallback(){
     console.log("Disconnected");
+    this._tooltipIcon.removeEventListener("mouseenter", this._showTooltip);
+    this._tooltipIcon.removeEventListener("mouseleave", this._showTooltip);
+
+  }
+
+  _render(){
+    let tooltipContainer = this.shadowRoot.querySelector("div");
+    if (this._tooltipVisible ) {
+      tooltipContainer = document.createElement("div");
+      tooltipContainer.textContent = this._tooltipText;
+      this.shadowRoot.appendChild(tooltipContainer);
+    } else {
+      if (tooltipContainer) {
+        this.shadowRoot.removeChild(tooltipContainer);
+      }
+    }
   }
 
 // Naming convention for functions called within a class
   _showTooltip(){
-    this._tooltipContainer = document.createElement("div");
-    this._tooltipContainer.textContent = this._tooltipText;
-    this.shadowRoot.appendChild(this._tooltipContainer);
+    this._tooltipVisible = true;
+    this._render();
   }
 
   _hideToolTip(){
-    this.shadowRoot.removeChild(this._tooltipContainer);
+    this._tooltipVisible = false;
+    this._render();
   }
 
 }
